d580c78e23262dd4130b4dfc8ed3d6a8ed199ebd
MiguelAngel-tech <D24390230@huauchinango.tecnm.mx>
2025-11-07 13:27:49 +0000

    se corrigieron errores entre operaciones

--- Modelo_EOQ_Clasico.java
+++ Modelo_EOQ_Clasico.java
@@ -3,14 +3,14 @@ public class Modelo_EOQ_Clasico{
     public static void main(String []args){
         Scanner lec = new Scanner(System.in);
         System.out.println("Ingrese la demanda del producto: ");
-        double d = lec.nextInt();
+        double d = lec.nextDouble();
         System.out.println("Ingrese el costo de la demanda: ");
-        double k = lec.nextInt();
+        double k = lec.nextDouble();
         System.out.println("Ingrese el costo de almacen del producto: ");
-        double h = lec.nextInt();
+        double h = lec.nextDouble();
         double y = 0;
         double resultado = Operaciones(d,k,h, y);
-        double entrega = Tiempo(y, d);
+        double entrega = Tiempo(resultado, d);
         System.out.println("El tamaño optimo del pedido es: " + resultado);
         System.out.println("El tiempo entre pedidos es: " + entrega);
     }
