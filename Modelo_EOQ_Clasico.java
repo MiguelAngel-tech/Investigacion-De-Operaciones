0267b8b2a1f18b93d8daf196f9179d28e1cf4e00
MiguelAngel-tech <D24390230@huauchinango.tecnm.mx>
2025-11-07 13:02:04 +0000

    Creacion de variables que se ocuparan para las operaciones deseadas

---
+++ Modelo_EOQ_Clasico.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Modelo_EOQ_Clasico{
+    public static void main(String []args){
+        Scanner lec = new Scanner(System.in);
+        System.out.println("Ingrese la demanda del producto: ");
+        int d = lec.nextInt();
+        System.out.println("Ingrese el costo de la demanda: ");
+        int k = lec.nextInt();
+        System.out.println("Ingrese el costo de almacen del producto: ");
+        int h = lec.nextInt();
+        
+    }
+}
