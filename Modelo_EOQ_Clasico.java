21f050bef5cd2c579dd58f5094d264678e3c327a
MiguelAngel-tech <D24390230@huauchinango.tecnm.mx>
2025-11-10 17:56:53 +0000

    se ordenó en un ciclo do while para mostra la opcion del metodo eoq y para el inventario con descuento

--- Modelo_EOQ_Clasico.java
+++ Modelo_EOQ_Clasico.java
@@ -2,28 +2,44 @@ import java.util.Scanner;
 public class Modelo_EOQ_Clasico{
     public static void main(String []args){
         Scanner lec = new Scanner(System.in);
-        System.out.println("");
-        System.out.println("Ingrese la demanda del producto: ");
-        int d = lec.nextInt();
-        System.out.println("Ingrese el costo de la demanda: ");
-        int k = lec.nextInt();
-        System.out.println("Ingrese el costo de almacen del producto: ");
-        float h = lec.nextFloat();
-        System.out.println("Ingrese el tiempo de entrega: ");
-        int l = lec.nextInt();
-        float y = 0;
-        int le = 0;
-        int resultado = Operaciones(d,k,h, y);
-        float entrega = Tiempo(resultado, d);
-        int n = (int)(l/entrega);
-        float ptoReorden = (float) PDR(entrega, l, n, le, d);
-        System.out.println();
-        System.out.println("El tamaño optimo del pedido es: " + resultado);
-        System.out.println("El tiempo entre pedidos es: " + entrega);
-        System.out.println("");
-        System.out.println("Punto de reorden");
-        System.out.println("");
-        System.out.println("El punto de reorden es: " + ptoReorden);
+        System.out.println("\n \n \t \tMODELO EOQ CLASICO");
+        do{
+            System.out.println("\n \n \t \tMenu");
+            System.out.println("1: Modelo EOQ clasico");
+            System.out.println("2: Incentario con descuento");
+            System.out.println("3: Salir");
+            byte opcion = lec.nextByte();
+            switch (opcion){
+                case 1:
+                    System.out.println("Ingrese la demanda del producto: ");
+                    int d = lec.nextInt();
+                    System.out.println("Ingrese el costo de la demanda: ");
+                    int k = lec.nextInt();
+                    System.out.println("Ingrese el costo de almacen del producto: ");
+                    float h = lec.nextFloat();
+                    System.out.println("Ingrese el tiempo de entrega: ");
+                    int l = lec.nextInt();
+                    float y = 0;
+                    int le = 0;
+                    int resultado = Operaciones(d,k,h, y);
+                    float entrega = Tiempo(resultado, d);
+                    int n = (int)(l/entrega);
+                    float ptoReorden = (float) PDR(entrega, l, n, le, d);
+                    System.out.println();
+                    System.out.println("El tamaño optimo del pedido es: " + resultado);
+                    System.out.println("El tiempo entre pedidos es: " + entrega);
+                    System.out.println("");
+                    System.out.println("Punto de reorden");
+                    System.out.println("");
+                    System.out.println("El punto de reorden es: " + ptoReorden);
+
+                    break;
+                case 2:
+                    break;
+                    default:
+                    System.out.println("Gracias por usar el programa, vuelva pronto. :)");
+            }
+        }while(opcion!=4);
 
     }
     public static int Operaciones(int d, int k, float h, float y){
